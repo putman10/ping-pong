e9928d5d8a67114f778633c8986cedd4bc1c0bc7
Ryan Putman and Scott Fraser <scott@>
2018-06-26 16:41:38 -0700

    add final method that replaces numbers

--- Ping-pong.Tests/ModelTests/Ping-pongTests.cs
+++ Ping-pong.Tests/ModelTests/Ping-pongTests.cs
@@ -10,7 +10,7 @@ namespace Games.Tests
     public void PingPongGenerator_CreateRange_True()
     {
       Pingpong testGame = new Pingpong();
-      CollectionAssert.AreEqual(new int[]{0,1}, testGame.PingPongGenerator(2));
+      CollectionAssert.AreEqual(new int[]{0,1,2,3}, testGame.PingPongGenerator(4));
     }
 
     [TestMethod]
@@ -25,6 +25,22 @@ namespace Games.Tests
     {
       Pingpong testGame = new Pingpong();
       Assert.AreEqual(true, testGame.DivisibleByFive(5));
+    }
+
+    [TestMethod]
+    public void DivisibleByBoth_ReplacePingPong_Bool()
+    {
+      Pingpong testGame = new Pingpong();
+      Assert.AreEqual(true, testGame.DivisibleByBoth(15));
+    }
+
+    [TestMethod]
+    public void ReplaceLoop_ReplacePingPong_Void()
+    {
+      Pingpong testGame = new Pingpong();
+      int[] array = testGame.PingPongGenerator(4);
+      string[] test = new string[]{"pingpong","test","test","pong"};
+      CollectionAssert.AreEqual(test, testGame.ReplaceLoop(array));
     }
   }
 }
