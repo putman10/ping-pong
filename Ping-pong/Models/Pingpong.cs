39d2c2c7bb2d6bf67bfb4d25441e07fd4d4153c4
Ryan Putman and Scott Fraser <ryan@>
2018-06-26 16:44:47 -0700

    huzzah

--- Ping-pong/Models/Pingpong.cs
+++ Ping-pong/Models/Pingpong.cs
@@ -1,6 +1,7 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace PingPong
@@ -13,16 +14,66 @@ namespace PingPong
       return numbers;
     }
 
+    public string[] ReplaceLoop(int[] array)
+    {
+      string[] finalString = new string[4];
+      for (int i = 0; i < array.Length; i++){
+        if (DivisibleByBoth(i))
+        {
+          finalString[i] = "pingpong";
+        }
+        else if (DivisibleByThree(i))
+        {
+          finalString[i] = "pong";
+        }
+        else if (DivisibleByFive(i))
+        {
+          finalString[i] = "ping";
+        }
+        else
+        {
+          finalString[i] = "test";
+        }
+        Console.WriteLine(finalString[i]);
+      }
+      return finalString;
+    }
+
     public bool DivisibleByThree(int n)
     {
-    if (n % 3 == 0)
+      if (n % 3 == 0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+      }
+
+    public bool DivisibleByFive(int n)
     {
-    return true;
+      if (n % 5 == 0)
+      {
+      return true;
+      }
+      else
+      {
+        return false;
+      }
     }
-    else
+
+    public bool DivisibleByBoth(int n)
     {
+      if (n % 5 == 0 && n % 3 == 0)
+      {
+      return true;
+      }
+      else
+      {
       return false;
+      }
     }
-    }
+
   }
 }
